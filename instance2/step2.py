2f77b8e69e647c30017f86dd0e7b054db1e57df9
Hithesh Sreedhara <hitheshsreedhara@gmail.com>
2018-06-25 11:32:52 +0200

    Create step2.py

---
+++ instance2/step2.py
@@ -0,0 +1,13 @@
+from random import random
+from time import sleep
+from sys import exit
+
+print("Running step2... ")
+
+sleep(1800 + 3*3600*random())
+
+if (random() > 0.999):
+    print("FAILED!")
+    exit(1)
+
+print("DONE!")
